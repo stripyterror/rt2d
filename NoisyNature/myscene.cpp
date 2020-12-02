e3cccbaf6bbc62535e39f84f89ea7068befa6907
Lars Huiting <larshuiting@gmail.com>
2020-12-02 10:57:28 +0100

    Perlin noise added

--- NoisyNature/myscene.cpp
+++ NoisyNature/myscene.cpp
@@ -1,7 +1,7 @@
 /**
  * This class describes MyScene behavior.
  *
- * Copyright 2015 Your Name <you@yourhost.com>
+ * Copyright 2017 Rik Teerling <rik@onandoffables.com>
  */
 
 #include <fstream>
@@ -9,56 +9,115 @@
 
 #include "myscene.h"
 
+ //RGBAColor terrain[3] = { water, grass, wetgrass }; // debug
+RGBAColor terrain[3] = { water, grass, wetgrass }; // cheat
+
 MyScene::MyScene() : Scene()
 {
-	// start the timer.
 	t.start();
 
-	// create a single instance of MyEntity in the middle of the screen.
-	// the Sprite is added in Constructor of MyEntity.
-	myentity = new MyEntity();
-	myentity->position = Point2(SWIDTH/2, SHEIGHT/2);
+	srand((unsigned)time(nullptr));
 
-	// create the scene 'tree'
-	// add myentity to this Scene as a child.
-	this->addChild(myentity);
+	// Create a PerlinNoise object with a random permutation vector generated with seed
+	unsigned int seed = rand() % 1000;
+	//unsigned int seed = 242;
+	pn = new PerlinNoise(seed);
+
+	// container for Sprite with custom Texture
+	entity = new BasicEntity();
+	entity->position = Point2(SWIDTH / 2, SHEIGHT / 2);
+	entity->scale = Point2(8.0f, 8.0f);
+	// width, height, bitdepth, filter, wrap
+	PixelBuffer pixels = PixelBuffer(64, 64, 3, 0, 0);
+	entity->addDynamicSprite(&pixels);
+
+	contrast = true;
+	posterize = true;
+	colorize = true;
+
+	
+
+	this->addChild(entity);
+	
 }
-
 
 MyScene::~MyScene()
 {
-	// deconstruct and delete the Tree
-	this->removeChild(myentity);
+	this->removeChild(entity);
+	delete entity;
 
-	// delete myentity from the heap (there was a 'new' in the constructor)
-	delete myentity;
+	
+
+	delete pn;
 }
 
 void MyScene::update(float deltaTime)
 {
-	// ###############################################################
-	// Escape key stops the Scene
-	// ###############################################################
-	if (input()->getKeyUp(KeyCode::Escape)) {
-		this->stop();
-	}
+	
 
-	// ###############################################################
-	// Spacebar scales myentity
-	// ###############################################################
-	if (input()->getKeyDown(KeyCode::Space)) {
-		myentity->scale = Point(0.5f, 0.5f);
-	}
-	if (input()->getKeyUp(KeyCode::Space)) {
-		myentity->scale = Point(1.0f, 1.0f);
-	}
+	if (t.seconds() > 0.02f) {
+		PixelBuffer* pixels = entity->sprite()->texture()->pixels();
 
-	// ###############################################################
-	// Rotate color
-	// ###############################################################
-	if (t.seconds() > 0.0333f) {
-		RGBAColor color = myentity->sprite()->color;
-		myentity->sprite()->color = Color::rotate(color, 0.01f);
+		unsigned int width = pixels->width;
+		unsigned int height = pixels->height;
+
+		long counter = 0;
+		// Visit every pixel of the image and assign a color generated with Perlin noise
+		for (unsigned int i = 0; i < height; ++i) {     // y
+			for (unsigned int j = 0; j < width; ++j) {  // x
+				// map width and height between 0 and 1
+				double x = (double)j / ((double)width);
+				double y = (double)i / ((double)height);
+				double z = 0.0f;
+
+
+				// find a nice noise for our purpose
+				// pn->noise(xsize, ysize, zsize) * multiplier;
+				double a = pn->noise(1 * x, 1 * y, 1 * z) * 2;
+				double b = pn->noise(5 * x, 5 * y, 5 * z) * 3;
+				double c = pn->noise(9 * x, 9 * y, 9 * z) * 1;
+
+				// average of noises ("octaves")
+				double n = (a + b + c) / 6;
+
+				// make an int in range 0-255 from n
+				int p = floor(255 * n);
+
+				// the color. no contrast, not posterized, not colored.
+				RGBAColor color = RGBAColor(p, p, p, 255);
+
+				// map for contrast. Averaging above made the image duller
+				if (contrast) {
+					p = map(p, 48, 208, 0, 255); // WARNING magic numbers
+					color = RGBAColor(p, p, p, 255);
+				}
+
+				// posterize
+				if (posterize) {
+					int numcolors = 3; // ... and another one. Make sure there are enough colors in array above
+					p = map(p, 0, 255, 0, numcolors);
+					// colorize (only colorize if posterized)
+					if (colorize) {
+						color = terrain[p];
+					}
+					else {
+						// restore and set color
+						p = map(p, 0, numcolors, 0, 255);
+						color = RGBAColor(p, p, p, 255);
+					}
+				}
+
+				// color the pixel
+				pixels->data[counter + 0] = color.r;
+				pixels->data[counter + 1] = color.g;
+				pixels->data[counter + 2] = color.b;
+				if (pixels->bitdepth == 4) {
+					pixels->data[counter + 3] = color.a;
+				}
+				counter += pixels->bitdepth;
+			}
+		}
+
 		t.start();
 	}
 }
--- NoisyNature/myscene.h
+++ NoisyNature/myscene.h
@@ -1,37 +1,42 @@
-/**
- * Copyright 2015 Your Name <you@yourhost.com>
- *
- * @file myscene.h
- *
- * @brief description of MyScene behavior.
- */
-
 #ifndef MYSCENE_H
 #define MYSCENE_H
 
 #include <rt2d/scene.h>
+#include <rt2d/noise.h>
+#include <rt2d/util.h>
 
-#include "myentity.h"
+class BasicEntity : public Entity
+{
+public:
+	BasicEntity() : Entity() { };
+	virtual ~BasicEntity() { };
 
-/// @brief The MyScene class is the Scene implementation.
+	virtual void update(float deltaTime) { };
+};
+
+
+#define water RGBAColor(127, 127, 215)
+#define grass RGBAColor(100, 185, 90)
+#define wetgrass RGBAColor(64, 127, 64)
+
+
 class MyScene : public Scene
 {
 public:
-	/// @brief Constructor
 	MyScene();
-	/// @brief Destructor
 	virtual ~MyScene();
 
-	/// @brief update is automatically called every frame
-	/// @param deltaTime the elapsed time in seconds
-	/// @return void
 	virtual void update(float deltaTime);
 
 private:
-	/// @brief the rotating square in the middle of the screen
-	MyEntity* myentity;
-	/// @brief a Timer to rotate the color every n seconds
 	Timer t;
+	BasicEntity* entity;
+
+	PerlinNoise* pn;
+
+	bool contrast;
+	bool posterize;
+	bool colorize;
 };
 
-#endif /* SCENE00_H */
+#endif /* MYSCENE_H */
